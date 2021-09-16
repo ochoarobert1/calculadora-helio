b576531453e243ed74e6079b00201643250890af
Robert Ochoa <ochoa.robert1@gmail.com>
2021-09-16 17:16:55 -0400

    Added Metabox to Custom Post Type

--- calculadora-helio.php
+++ calculadora-helio.php
@@ -27,8 +27,116 @@
  * along with calculadora-helio. If not, see {URI to Plugin License}.
 */
 
-// CREATE HOOKS ON ACTIVATE / DEACTIVATE =======================================SA
+// CREATE HOOKS ON ACTIVATE / DEACTIVATE =======================================
 require_once('inc/activation_hook.php');
 require_once('inc/deactivation_hook.php');
 
 // MAIN CLASS CONTAINER ========================================================
+class Calculadora
+{
+    // =========================================================================
+    // MAIN CONSTRUCTOR
+    // =========================================================================
+    public function __construct()
+    {
+        add_action('add_meta_boxes', array($this, 'addMetaBox'));
+        add_action('save_post', array($this, 'saveMetaBox'));
+        add_filter('manage_medidas_posts_columns', array($this, 'custom_filter_posts_columns'));
+        add_action('manage_medidas_posts_custom_column', array($this, 'custom_medidas_column'), 10, 2);
+        add_filter('manage_edit-medidas_sortable_columns', array($this, 'custom_medidas_sortable_columns'));
+        add_action('pre_get_posts', array($this, 'custom_posts_orderby' ));
+    }
+
+    // =========================================================================
+    // ADD CUSTOM ADMIN COLUMN
+    // =========================================================================
+    public function custom_filter_posts_columns($columns)
+    {
+        unset($columns['date']);
+        $columns['cantidad_helio'] = __('Cantidad de Helio', 'calculadora-helio');
+        $columns['date'] = __('Fecha', 'wordpress');
+        return $columns;
+    }
+
+    // =========================================================================
+    // ADD SORTABLE BEHAVIOUR TO COLUMN
+    // =========================================================================
+    public function custom_medidas_sortable_columns($columns)
+    {
+        $columns['cantidad_helio'] = 'cantidad_helio';
+        return $columns;
+    }
+
+    // =============================================================================
+    // ADD PRE GET POST TO SORTABLE ADMIN COLUMN
+    // =============================================================================
+    public function custom_posts_orderby($query)
+    {
+        if (! is_admin() || ! $query->is_main_query()) {
+            return;
+        }
+      
+        if ('cantidad_helio' === $query->get('orderby')) {
+            $query->set('orderby', 'meta_value');
+            $query->set('meta_key', 'cantidad_helio');
+            $query->set('meta_type', 'numeric');
+        }
+    }
+      
+    // =========================================================================
+    // ADD VALUE TO CUSTOM ADMIN COLUMN
+    // =========================================================================
+    public function custom_medidas_column($column, $post_id)
+    {
+        // Image column
+        if ('cantidad_helio' === $column) {
+            $cantidad = get_post_meta($post_id, 'cantidad_helio', true);
+
+            if ($cantidad != '') {
+                echo number_format($cantidad, 2, ',', '.') . ' m3';
+            } else {
+                _e('Cantidad no especificada', 'calculadora-helio');
+            }
+        }
+    }
+
+    // =========================================================================
+    // ADD METABOX FOR MEASURES POST TYPE
+    // =========================================================================
+    public function addMetaBox()
+    {
+        add_meta_box(
+            'medidas_meta_box',
+            esc_html__('Información Extra', 'calculadora-helio'),
+            array($this, 'htmlMetaBox'),
+            array('medidas')
+        );
+    }
+
+    // =========================================================================
+    // SAVE METABOX FOR MEASURES POST TYPE
+    // =========================================================================
+    public function saveMetaBox($post_id)
+    {
+        if (array_key_exists('cantidad_helio', $_POST)) {
+            update_post_meta(
+                $post_id,
+                'cantidad_helio',
+                $_POST['cantidad_helio']
+            );
+        }
+    }
+
+    // =========================================================================
+    // HTML METABOX FOR MEASURES POST TYPE
+    // =========================================================================
+    public function htmlMetaBox($post)
+    {
+        $value = get_post_meta($post->ID, 'cantidad_helio', true); ?>
+<label for="cantidad_helio"><?php _e('Cantidad de helio necesario: (Utilice sólo numeros)', 'calculadora-helio'); ?></label>
+<input type="text" name="cantidad_helio" id="cantidad_helio" class="postbox" style="margin: 5px 0;" value="<?php echo $value; ?>" />
+<?php
+    }
+}
+
+new Calculadora;
