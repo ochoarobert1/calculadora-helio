5885797c8911b858f0727c1ae171d5cfc9b94c4f
Robert Ochoa <ochoa.robert1@gmail.com>
2021-09-17 13:22:23 -0400

    Correccion de Error en JS por no checkear existencia

--- js/functions.js
+++ js/functions.js
@@ -10,48 +10,52 @@ function proyectoCustomLoad() {
 
 document.addEventListener("DOMContentLoaded", proyectoCustomLoad, false);
 
-addItems.addEventListener('click', function(e) {
-    e.preventDefault();
-    /* SEND AJAX */
-    var info = 'action=add_calculadora_items';
-    newRequest = new XMLHttpRequest();
-    newRequest.open('POST', custom_admin_url.ajax_url, true);
-    newRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    newRequest.onload = function() {
-        var respuesta = JSON.parse(newRequest.response);
-        var ajaxResponse = document.getElementById('calculadoraWrapper');
-        ajaxResponse.insertAdjacentHTML('beforeend', respuesta.data);
-        var deleteItems = document.getElementsByClassName('deleteItems');
-        for (item of deleteItems) {
-            item.addEventListener('click', delete_calculadora_item, false);
-        }
-    };
-    newRequest.send(info);
-});
+if (addItems) {
+    addItems.addEventListener('click', function(e) {
+        e.preventDefault();
+        /* SEND AJAX */
+        var info = 'action=add_calculadora_items';
+        newRequest = new XMLHttpRequest();
+        newRequest.open('POST', custom_admin_url.ajax_url, true);
+        newRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+        newRequest.onload = function() {
+            var respuesta = JSON.parse(newRequest.response);
+            var ajaxResponse = document.getElementById('calculadoraWrapper');
+            ajaxResponse.insertAdjacentHTML('beforeend', respuesta.data);
+            var deleteItems = document.getElementsByClassName('deleteItems');
+            for (item of deleteItems) {
+                item.addEventListener('click', delete_calculadora_item, false);
+            }
+        };
+        newRequest.send(info);
+    });
+}
 
-calculadoraBtn.addEventListener('click', function(e) {
-    var passd = true;
-    e.preventDefault();
-    var formEl = document.forms.calculadoraForm;
-    var formData = new FormData(formEl);
-    var cantidad_globos = formData.getAll('cantidad_globos[]');
-    var modelo_globo = formData.getAll('modelo_globo[]');
-    if (modelo_globo.length === 0) {
-        passd = false;
-    } else {
-        for (let index = 0; index < cantidad_globos.length; index++) {
-            if (modelo_globo[index] == null) {
-                passd = false;
+if (calculadoraBtn) {
+    calculadoraBtn.addEventListener('click', function(e) {
+        var passd = true;
+        e.preventDefault();
+        var formEl = document.forms.calculadoraForm;
+        var formData = new FormData(formEl);
+        var cantidad_globos = formData.getAll('cantidad_globos[]');
+        var modelo_globo = formData.getAll('modelo_globo[]');
+        if (modelo_globo.length === 0) {
+            passd = false;
+        } else {
+            for (let index = 0; index < cantidad_globos.length; index++) {
+                if (modelo_globo[index] == null) {
+                    passd = false;
+                }
             }
         }
-    }
-    if (passd == true) {
-        document.getElementById('calculadoraError').classList.add('no-display');
-        mainCalculate(cantidad_globos, modelo_globo);
-    } else {
-        document.getElementById('calculadoraError').classList.remove('no-display');
-    }
-});
+        if (passd == true) {
+            document.getElementById('calculadoraError').classList.add('no-display');
+            mainCalculate(cantidad_globos, modelo_globo);
+        } else {
+            document.getElementById('calculadoraError').classList.remove('no-display');
+        }
+    });
+}
 
 function delete_calculadora_item() {
     var currentItem = this.parentElement;
